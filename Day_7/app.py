f8a40203285a8df3291d2ab88ff5ff58e0efd1c6
kolli <brahmam3666@gmail.com>
2026-04-14 21:00:09 +0530

    Day 7 Flask API with Docker

---
+++ Day_7/app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "MLOps API is Running"
+
+@app.route('/predict/<int:num>')
+def predict(num):
+    result = num * 2
+    return {"prediction": result}
+
+if __name__ == "__main__":
+    app.run(host = "0.0.0.0", port = 5000)
+    
